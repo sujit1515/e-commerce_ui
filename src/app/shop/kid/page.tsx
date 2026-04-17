150b1d32e6d74e379f6be0234f50d16c03b05511
Sujit Pattnaik <sujitpattnaik41@gmail.com>
2026-04-18 00:30:40 +0530

    slove the path issue on the kid section

--- src/app/shop/kid/page.tsx
+++ src/app/shop/kid/page.tsx
@@ -1,13 +1,14 @@
 import Footer from '@/components/Common/Footer';
 import Navbar from '@/components/Common/Navbar';
-import MensCollectionPage from '@/components/Shop/Men/MensCollectionPage';
+import KidsCollectionPage from '@/components/Shop/Kid/Kidscollectionpage';
+
 import React from 'react';
 
 export default function MensCollectionpage() {
   return (
     <div>
       <Navbar/>
-      <MensCollectionPage/>
+       <KidsCollectionPage/>
       <Footer/>
     </div>
   )
